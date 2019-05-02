7920345cf643aacb5d353a4f5eae204a43f54158
siasty <siasty3@gmail.com>
2019-05-02 08:20:58 +0200

    1

--- UI5/wwwroot/ui5/controller/Overview.controller.js
+++ UI5/wwwroot/ui5/controller/Overview.controller.js
@@ -6,80 +6,19 @@ sap.ui.define([
 
         return Controller.extend("ui5.controller.Overview", {
 
-        onInit: function () {
-            this.getView().addStyleClass("sapUiSizeCompact"); // make everything inside this View appear in Compact mode
+            onInit: function () {
+                this.oModel = new ODataModel({
+                    groupId: "$direct",
+                    synchronizationMode: "None",
+                    serviceUrl: 'https://localhost:5001/odata/'
+                });
 
-            this.getView().bindElement("/Books");
+                this.getView().setModel(this.oModel, 'items');
 
-        },
+                this.getView().bindElement({ path: "/Books" });
 
-        onErrorCall: function (oError) {
-            if (oError.statusCode === 500 || oError.statusCode === 400 || oError.statusCode === "500" || oError.statusCode === "400") {
-                var errorRes = JSON.parse(oError.responseText);
-                if (!errorRes.error.innererror) {
-                    sap.m.MessageBox.alert(errorRes.error.message.value);
-                } else {
-                    if (!errorRes.error.innererror.message) {
-                        sap.m.MessageBox.alert(errorRes.error.innererror.toString());
-                    } else {
-                        sap.m.MessageBox.alert(errorRes.error.innererror.message);
-                    }
-                }
-                return;
-            } else {
-                sap.m.MessageBox.alert(oError.response.statusText);
-                return;
-            }
-
-        },
-        /**The following methods could be added to a reusable base controller
-                 * Convenience method for accessing the router in every controller of the application.
-                 * @public
-                 * @returns {sap.ui.core.routing.Router} the router for this component
-                 */
-        getRouter: function () {
-            return this.getOwnerComponent().getRouter();
-        },
-
-        /**
-         * Convenience method for getting the view model by name in every controller of the application.
-         * @public
-         * @param {string} sName the model name
-         * @returns {sap.ui.model.Model} the model instance
-         */
-        getModel: function (sName) {
-            return this.getView().getModel(sName);
-        },
-
-        /**
-         * Convenience method for setting the view model in every controller of the application.
-         * @public
-         * @param {sap.ui.model.Model} oModel the model instance
-         * @param {string} sName the model name
-         * @returns {sap.ui.mvc.View} the view instance
-         */
-        setModel: function (oModel, sName) {
-            return this.getView().setModel(oModel, sName);
-        },
-
-
-        /**
-         * Event handler for navigating back.
-         * It there is a history entry we go one step back in the browser history
-         * If not, it will replace the current entry of the browser history with the master route.
-         * @public
-         */
-        onNavBack: function () {
-            var sPreviousHash = History.getInstance().getPreviousHash();
-
-            if (sPreviousHash !== undefined) {
-                history.go(-1);
-            } else {
-                this.getRouter().navTo("master", {}, true);
-            }
-        }
-
-    });
+            },
+        });
 
        
 });
