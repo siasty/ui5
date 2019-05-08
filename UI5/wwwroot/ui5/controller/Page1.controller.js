922f91947a30702ed850e0319b6b2a1dd5131b3b
Krzysztof Śliwa <sliwkipoczta@gmail.com>
2019-05-08 07:57:28 +0200

    stronka1

---
+++ UI5/wwwroot/ui5/controller/Page1.controller.js
@@ -0,0 +1,55 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function(BaseController, MessageBox, Utilities, History) {
+	"use strict";
+
+	return BaseController.extend("com.sap.build.standard.untitledPrototype.controller.Page1", {
+		handleRouteMatched: function(oEvent) {
+			var sAppId = "App5cd26c9676181841b6bf8f12";
+
+			var oParams = {};
+
+			if (oEvent.mParameters.data.context) {
+				this.sContext = oEvent.mParameters.data.context;
+
+			} else {
+				if (this.getOwnerComponent().getComponentData()) {
+					var patternConvert = function(oParam) {
+						if (Object.keys(oParam).length !== 0) {
+							for (var prop in oParam) {
+								if (prop !== "sourcePrototype") {
+									return prop + "(" + oParam[prop][0] + ")";
+								}
+							}
+						}
+					};
+
+					this.sContext = patternConvert(this.getOwnerComponent().getComponentData().startupParameters);
+
+				}
+			}
+
+			var oPath;
+
+			if (this.sContext) {
+				oPath = {
+					path: "/" + this.sContext,
+					parameters: oParams
+				};
+				this.getView().bindObject(oPath);
+			}
+
+		},
+		avatarInitialsFormatter: function(sTextValue) {
+			return typeof sTextValue === 'string' ? sTextValue.substr(0, 2) : undefined;
+
+		},
+		onInit: function() {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("Page1").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+
+		}
+	});
+}, /* bExport= */ true);
